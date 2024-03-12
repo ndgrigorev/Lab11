35be629ed279f3ca10be72443ef1c801f5b351f6
ndgrigorev <nikgrigorev0808@gmail.com>
2024-03-12 18:13:06 +0500

    initial commit

---
+++ Lab11/Program.cs
@@ -0,0 +1,239 @@
+﻿using Lab10_Library;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Часть1
+            Stack animals = new Stack();
+            for (int i = 0; i < 2; i++)
+            {
+                Animals animal = new Animals();
+                animal.RandomInit();
+                animals.Push(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Mammals animal = new Mammals();
+                animal.RandomInit();
+                animals.Push(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Cats animal = new Cats();
+                animal.RandomInit();
+                animals.Push(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Fishes animal = new Fishes();
+                animal.RandomInit();
+                animals.Push(animal);
+            }
+            foreach (object obj in animals)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+            //запросы
+            double sum = 0;
+            int count = 0;
+            foreach (Animals animal in animals)
+            {
+                if (animal is Mammals mammal)
+                {
+                    sum += mammal.Weight;
+                    count++;
+                }
+            }
+            Console.WriteLine($"Средний вес млекопитающих = {Math.Round(sum / count, 2)}");
+            string[] names = new string[animals.Count];
+            int j = 0;
+            foreach (Animals animal in animals)
+            {
+                Cats cat = animal as Cats;
+                if (cat != null)
+                {
+                    if (cat.Color != "рыжий")
+                    {
+                        names[j++] = cat.Name;
+                    }
+                }
+            }
+            for (int i = 0; i < j; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            int maxAge = -1;
+            string name = null;
+            foreach (Animals animal in animals)
+            {
+                if (typeof(Fishes) == animal.GetType())
+                {
+                    Fishes fish = (Fishes)animal;
+                    if (fish.Age > maxAge)
+                    {
+                        maxAge = fish.Age;
+                        name = fish.Name;
+                    }
+                }
+            }
+            Console.WriteLine($"Самая старшая рыба - {name}");
+            Console.WriteLine();
+            Stack animalsClone = (Stack)animals.Clone();
+            Animals example = new Animals(100, "Образец", "мужской", 12);
+            animals.Push(example);
+            //сортировка и поиск
+            object[] animalsArr = new object[animals.Count];
+            animalsArr = animals.ToArray();
+            int k = 0;
+            foreach (Animals animal in animals)
+            {
+                animalsArr[k++] = animal;
+            }
+            Array.Sort(animalsArr);
+            animals.Clear();
+            for (int i = animalsArr.Length - 1;i >= 0; i--)
+            {
+                animals.Push(animalsArr[i]);
+            }
+            foreach (object obj in animals)
+            {
+                Console.WriteLine(obj);
+            }
+            if (animals.Contains(example))
+            {
+                int p = 0;
+                foreach(Animals animal in animals)
+                {
+                    if(animal == example)
+                    {
+                        Console.WriteLine($"Искомый элемент найден на позиции {p + 1}");
+                        break;
+                    }
+                    p++;
+                }
+            }
+            Console.WriteLine();
+            //удаление
+            Console.WriteLine("Удаление");
+            Stack temp = new Stack();
+            foreach (Animals animal in animals)
+            {
+                if (!(animal.Equals(example)))
+                {
+                    temp.Push(animal);
+                }
+            }
+            animals.Clear();
+            foreach(Animals animal in temp)
+            {
+                animals.Push(animal);
+            }
+            foreach (Animals animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+            //Часть2
+            List<Animals> animalsList = new List<Animals>();
+            for (int i = 0; i < 2; i++)
+            {
+                Animals animal = new Animals();
+                animal.RandomInit();
+                animalsList.Add(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Mammals animal = new Mammals();
+                animal.RandomInit();
+                animalsList.Add(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Cats animal = new Cats();
+                animal.RandomInit();
+                animalsList.Add(animal);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Fishes animal = new Fishes();
+                animal.RandomInit();
+                animalsList.Add(animal);
+            }
+            foreach (Animals obj in animalsList)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+            //запросы
+            sum = 0;
+            count = 0;
+            foreach (Animals animal in animalsList)
+            {
+                if (animal is Mammals mammal)
+                {
+                    sum += mammal.Weight;
+                    count++;
+                }
+            }
+            Console.WriteLine($"Средний вес млекопитающих  = {Math.Round(sum / count, 2)}");
+            string[] names2 = new string[animalsList.Count];
+            j = 0;
+            foreach (Animals animal in animalsList)
+            {
+                Cats cat = animal as Cats;
+                if (cat != null)
+                {
+                    if (!(cat.Color == "рыжий"))
+                    {
+                        names2[j++] = cat.Name;
+                    }
+                }
+            }
+            for (int i = 0; i < j; i++)
+            {
+                Console.WriteLine(names2[i]);
+            }
+            maxAge = -1;
+            name = null;
+            foreach (Animals animal in animalsList)
+            {
+                if (typeof(Fishes) == animal.GetType())
+                {
+                    Fishes fish = (Fishes)animal;
+                    if (fish.Age > maxAge)
+                    {
+                        maxAge = fish.Age;
+                        name = fish.Name;
+                    }
+                }
+            }
+            Console.WriteLine($"Самая старшая морская рыба - {name}");
+            Console.WriteLine();
+            //сортировка и поиск
+            Animals example2 = new Animals(100, "Рекс", "мужской", 12);
+            animalsList[0] = example2;
+            animalsList.Sort();
+            foreach (Animals obj in animalsList)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine($"Элемент находится на позиции {animalsList.BinarySearch(example2) + 1}");
+            animalsList.Remove(example2);
+            Console.WriteLine();
+            foreach (Animals obj in animalsList)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            //часть3
+            TestCollections time = new TestCollections(1000);
+            time.Testing();
+        }
+    }
+}
---
+++ Lab11/TestCollections.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Lab10_Library;
+using System.Security.Cryptography;
+
+namespace Lab11
+{
+    internal class TestCollections
+    {
+        public Dictionary <Animals, Mammals> collection1 = new Dictionary<Animals, Mammals> ();
+        public Dictionary<string, Mammals> collection2 = new Dictionary<string, Mammals>();
+        public SortedSet<Mammals> collection3 = new SortedSet <Mammals> ();
+        public SortedSet<string> collection4 = new SortedSet <string> ();
+
+        Mammals? first, middle, last, noexist;
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        bool mainCheck = true;
+
+        public TestCollections(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Mammals animal = new Mammals();
+                animal.RandomInit();
+                animal.Name = i.ToString() + animal.Name;
+                collection1.Add(animal.GetBase(), animal);
+                collection2.Add(animal.GetBase().ToString(), animal);
+                collection3.Add(animal);
+                collection4.Add(animal.ToString());
+                if (i == 0)
+                {
+                    first = (Mammals)animal.Clone();
+                }
+                if (i == length / 2)
+                {
+                    middle = (Mammals)animal.Clone();
+                }
+                if (i == length - 1)
+                {
+                    last = (Mammals)animal.Clone();
+                }
+            }
+            Mammals animal2 = new Mammals();
+            animal2.RandomInit();
+            animal2.Id.Number = 0;
+            noexist = (Mammals)animal2.Clone();
+        }
+        public void Testing()
+        {
+            //первая коллекция
+            double time = TestCollection1(first);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Первый элемент в collection1 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый элемент в collection1 не найден за {time}");
+            }
+            time = TestCollection1(middle);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Центральный элемент в collection1 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Центральный элемент в collection1 не найден за {time}");
+            }
+            time = TestCollection1(last);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Последний элемент в collection1 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний элемент в collection1 не найден за {time}");
+            }
+            time = TestCollection1(noexist);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Несуществующий элемент в collection1 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Несуществующий элемент в collection1 не найден за  {time}");
+            }
+            //вторая коллекция
+            Console.WriteLine();
+            time = TestCollection2(first);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Первый элемент в collection2 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый элемент в collection2 не найден за {time}");
+            }
+            time = TestCollection2(middle);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Центральный элемент в collection2 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Центральный элемент в collection2 не найден за {time}");
+            }
+            time = TestCollection2(last);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Последний элемент в collection2 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний элемент в collection2 не найден за {time}");
+            }
+            time = TestCollection2(noexist);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Несуществующий элемент в collection2 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Несуществующий элемент в collection2 не найден за  {time}");
+            }
+            //третья коллекция
+            Console.WriteLine();
+            time = TestCollection3(first);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Первый элемент в collection3 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый элемент в collection3 не найден за {time}");
+            }
+            time = TestCollection3(middle);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Центральный элемент в collection3 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Центральный элемент в collection3 не найден за {time}");
+            }
+            time = TestCollection3(last);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Последний элемент в collection3 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний элемент в collection3 не найден за {time}");
+            }
+            time = TestCollection3(noexist);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Несуществующий элемент в collection3 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Несуществующий элемент в collection3 не найден за  {time}");
+            }
+            //третья коллекция
+            Console.WriteLine();
+            time = TestCollection4(first);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Первый элемент в collection4 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Первый элемент в collection4 не найден за {time}");
+            }
+            time = TestCollection4(middle);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Центральный элемент в collection4 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Центральный элемент в collection4 не найден за {time}");
+            }
+            time = TestCollection4(last);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Последний элемент в collection4 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Последний элемент в collection4 не найден за {time}");
+            }
+            time = TestCollection4(noexist);
+            if (mainCheck)
+            {
+                Console.WriteLine($"Несуществующий элемент в collection4 найден за {time}");
+            }
+            else
+            {
+                Console.WriteLine($"Несуществующий элемент в collection4 не найден за {time}");
+            }
+        }
+        private double TestCollection1(Mammals? animal)
+        {
+            double time = 0;
+            mainCheck = true;
+            bool check = true;
+            bool check2 = true;
+            for (int i = 0; i < 50; i++)
+            {
+                sw.Restart();
+                check = collection1.ContainsKey(animal.GetBase());
+                check2 = collection1.ContainsValue(animal);
+                sw.Stop();
+                time += sw.ElapsedTicks;
+                if (!(check && check2))
+                {
+                    mainCheck = false;
+                }
+            }
+            return time / 50.0;
+        }
+        private double TestCollection2(Mammals? animal)
+        {
+            double time = 0;
+            mainCheck = true;
+            bool check = true;
+            bool check2 = true;
+            for (int i = 0; i < 50; i++)
+            {
+                sw.Restart();
+                check = collection2.ContainsKey(animal.GetBase().ToString());
+                check2 = collection2.ContainsValue(animal);
+                sw.Stop();
+                time += sw.ElapsedTicks;
+                if (!(check && check2))
+                {
+                    mainCheck = false;
+                }
+            }
+            return time / 50.0;
+        }
+        private double TestCollection3(Mammals? animal)
+        {
+            double time = 0;
+            mainCheck = true;
+            bool check = true;
+            for (int i = 0; i < 50; i++)
+            {
+                sw.Restart();
+                check = collection3.Contains(animal);
+                sw.Stop();
+                time += sw.ElapsedTicks;
+                if (!check)
+                {
+                    mainCheck = false;
+                }
+            }
+            return time / 50.0;
+        }
+        private double TestCollection4(Mammals? animal)
+        {
+            double time = 0;
+            mainCheck = true;
+            bool check = true;
+            for (int i = 0; i < 50; i++)
+            {
+                sw.Restart();
+                check = collection4.Contains(animal.ToString());
+                sw.Stop();
+                time += sw.ElapsedTicks;
+                if (!check)
+                {
+                    mainCheck = false;
+                }
+            }
+            return time / 50.0;
+        }
+    }
+}
